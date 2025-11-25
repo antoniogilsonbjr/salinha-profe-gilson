fb41dc600caa718404aa8a6d0d7c8e648be62e15
antoniogilsonbjr <gilsonantonio.bjr@gmail.com>
2025-11-24 21:56:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+import { PeerServer } from 'peer';
 
+const port = process.env.PORT ? parseInt(process.env.PORT) : 9000;
+
+console.log(`Iniciando servidor PeerJS na porta ${port}...`);
+
+const peerServer = PeerServer({
+  port: port,
+  path: '/myapp', // Caminho importante para a conexão
+  proxied: true,  // Essencial para funcionar atrás do HTTPS do Render
+  allow_discovery: true,
+  corsOptions: {
+      origin: '*',
+  }
+});
+
+console.log('Servidor rodando!');
